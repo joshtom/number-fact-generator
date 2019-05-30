3cc1ea20bde0181e5d34b09e4aab5fd9444e1bc2
Joshua <olajidejoshua4real@gmail.com>
2019-05-30 15:59:26 +0100

    A checkbox added that allows user to search random fact based on year

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
           let fact = document.querySelector("#fact");
           let factText = document.querySelector("#factText");
           let numberInput = document.querySelector("#numberInput");
-          const getFactAjax = () => {
-        //     // Getting the user input
-            let number = numberInput.value;
-            // Getting request from the api
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', 'http://numbersapi.com/'+number);
+        //   const getFactAjax = () => {
+        // //     // Getting the user input
+        //     let number = numberInput.value;
+        //     // Getting request from the api
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open('GET', 'http://numbersapi.com/'+number);
 
-            xhr.onload = function (){
-                if (this.status == 200 && number != ""){ //The alternative is number != ""
-                    // console.log(this.responseText);
-                    fact.style.display = 'block';
-                    factText.innerText = this.responseText;
-                }
-            }
-            xhr.send();
-        }
+        //     xhr.onload = function (){
+        //         if (this.status == 200 && number != ""){ //The alternative is number != ""
+        //             // console.log(this.responseText);
+        //             fact.style.display = 'block';
+        //             factText.innerText = this.responseText;
+        //         }
+        //     }
+        //     xhr.send();
+        // }
         
                 // Using the fetch API to get the value 
                 var getFactFetch = () => {
@@ -27,12 +27,41 @@
                         if (number != ""){
                             fact.style.display = 'block';
                             factText.innerText = data;
+                        } else{
+                            fact.style.display = 'none';
                         }
                     })
                     .catch(err => console.log(err));
                 }
-          
-          numberInput.addEventListener('input', getFactFetch);
+
+                // Trigger the checkbox to make the input box display random text based on the entered year
+                function getRandomYear() {
+                var check = document.getElementById("year");
+                if (check.checked)
+                    {
+                        var getFactFetch = () => {
+                            let number = numberInput.value;
+                            fetch('http://numbersapi.com/'+number+'/year')
+                            .then(response => response.text())
+                            .then(data => {
+                                if (number != ""){
+                                    fact.style.display = 'block';
+                                    factText.innerText = data;
+                                } else{
+                                    fact.style.display = 'none';
+                                }
+                            })
+                            .catch(err => console.log(err));
+                        }
+                        numberInput.addEventListener('input', getFactFetch);
+                    }
+                else 
+                    {
+                        location.reload();
+                    }
+                }
+
+        //   numberInput.addEventListener('input', getFactFetch);
 
           
       
